2fbe6f6c63a0206aa1f7929eb17f301bf9610b36
IsaacOQ <isaacdiaztrejo@gmail.com>
2023-06-01 09:13:15 -0600

    añadir operaciones.cpp

---
+++ operaciones.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numA = 5, numB = 4;
+    cout<<"La suma de " << numA << " + " << numB << " es: " << numA + numB;
+
+    return 0;
+
+}
